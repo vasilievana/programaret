27e2d916e526c6eb9e04936ac0fee7bb7f5c3d2b
vasilievana <103123346+vasilievana@users.noreply.github.com>
2023-12-02 21:24:57 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var manuBtn = document.getElementById('manuBtn')
+            var sideNav = document.getElementById('sideNav')
+            var menu = document.getElementById('menu')
+            sideNav.style.right = "-250px";
+            manuBtn.onclick = function () {
+                if (sideNav.style.right == "-250px") {
+                    sideNav.style.right = "0";
+                    menu.src = "images/close.png";
+                }
+                else {
+                    sideNav.style.right = "-250px";
+                    menu.src = "images/menu.png";
+    
+                }
+            }
+            // Smoth scrolling js
+            var scroll = new SmoothScroll('a[href*="#"]', {
+                speed: 1000,
+                speedAsDuration: true
+            });
